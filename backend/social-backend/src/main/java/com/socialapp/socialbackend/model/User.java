c7923be63f19274cc89011958531972696b35984
iam-abhinavmishra <thisisabhinavmishra@gmail.com>
2026-08-27 06:15:34 +0530

    Prevent duplicate email registration

--- backend/social-backend/src/main/java/com/socialapp/socialbackend/model/User.java
+++ backend/social-backend/src/main/java/com/socialapp/socialbackend/model/User.java
@@ -21,10 +21,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = false)
     private String username;
 
-    @Column(nullable = false, unique = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     private String password;
